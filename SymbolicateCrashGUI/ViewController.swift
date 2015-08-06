f6daed3525dead6a3ba92b94cba99720ccfaa0ee
Arthur Wang <arthraim@gmail.com>
2015-08-06 15:57:06 +0800

    Initial Commit

---
+++ SymbolicateCrashGUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SymbolicateCrashGUI
+//
+//  Created by Arthur Wang on 8/6/15.
+//  Copyright (c) 2015 YangApp.com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ SymbolicateCrashGUITests/SymbolicateCrashGUITests.swift
@@ -0,0 +1,36 @@
+//
+//  SymbolicateCrashGUITests.swift
+//  SymbolicateCrashGUITests
+//
+//  Created by Arthur Wang on 8/6/15.
+//  Copyright (c) 2015 YangApp.com. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class SymbolicateCrashGUITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
